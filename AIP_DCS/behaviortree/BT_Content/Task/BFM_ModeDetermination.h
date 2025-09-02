c0777fa6ef938bbba7a8bd198af9618af608b133
eunhoyou <sukjw97@gmail.com>
2025-09-03 07:22:47 +0900

    Initial commit

---
+++ AIP_DCS/behaviortree/BT_Content/Task/BFM_ModeDetermination.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../../behaviortree_cpp_v3/action_node.h"
+#include "../../behaviortree_cpp_v3/bt_factory.h"
+#include "../../../Geometry/Vector3.h"
+#include "../Functions.h"
+#include "../BlackBoard/CPPBlackBoard.h"
+
+using namespace BT;
+
+namespace Action
+{
+    class BFM_ModeDetermination : public SyncActionNode
+    {
+    private:
+
+    public:
+        BFM_ModeDetermination(const std::string& name, const NodeConfiguration& config) : SyncActionNode(name, config)
+        {
+        }
+
+        ~BFM_ModeDetermination()
+        {
+        }
+
+        static PortsList providedPorts();
+        NodeStatus tick() override;
+    };
+}
